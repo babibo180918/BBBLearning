8f9c1a8225c46c694f084301521b4cdd19ab69ab
Nhan Nguyen <nguyendtnhan@gmail.com>
2020-09-05 01:20:53 +0900

    - Add retrain function and enable to save parameter to output model

--- BBBLearning/lr/lr.py
+++ BBBLearning/lr/lr.py
@@ -22,7 +22,7 @@ def parameters_initialize(size, initializer = "zero"):
         w = np.random.randn(size, 1)*np.sqrt(2/size)
     return w, b
 
-def train(model, X, Y, iteration, learning_rate, beta1, beta2, epsilon, print_cost=False):
+def train(model, X, Y, iteration, learning_rate, beta1, beta2, epsilon, model_path, print_cost=False):
     size = model["size"]
     cost_function = model["cost_function"]
     optimizer = model["optimizer"]
@@ -55,9 +55,63 @@ def train(model, X, Y, iteration, learning_rate, beta1, beta2, epsilon, print_co
             costs.append(cost)
             if print_cost:
                 print("Cost after iteration {}: {}".format(i, cost))
+            model["w"] = w
+            model['b'] = b
+            with open(model_path, 'wb') as out_file:
+                np.save(out_file, model)
+                out_file.close()
+                print("Model saved!")
     params = {"w":w,
               "b":b}
     return params, costs
+
+def retrain(model, X, Y, iteration, learning_rate, beta1, beta2, epsilon, model_path, print_cost=False):
+    size = model["size"]
+    cost_function = model["cost_function"]
+    optimizer = model["optimizer"]
+    initializer = model["initializer"]
+
+    costs = []
+    vgrads = {"vdw":0.0,
+              "vdb":0.0}
+    sgrads = {"sdw":0.0,
+              "sdb":0.0}
+    w = model['w']
+    b = model['b']
+    for i in range(iteration):
+        # compute gradient and cost
+        grads, cost = propagate(w, b, X, Y, cost_function)
+
+        vgrads["vdw"] = beta1*vgrads["vdw"] + (1-beta1)*grads["dw"]
+        vgrads["vdb"] = beta1*vgrads["vdb"] + (1-beta1)*grads["db"]
+        v_corrected = 1 - np.power(beta1, i)
+        
+        sgrads["sdw"] = beta2*sgrads["sdw"] + (1-beta2)*np.multiply(grads["dw"], grads["dw"])
+        sgrads["sdb"] = beta2*sgrads["sdb"] + (1-beta2)*np.multiply(grads["db"], grads["db"])
+        s_corrected = 1 - np.power(beta2, i)
+        
+        # update parameters
+        w, b = update(w, b, grads, vgrads, sgrads, learning_rate, epsilon, v_corrected, s_corrected, optimizer)
+
+        # save costs
+        if i % 100 == 0:
+            costs.append(cost)
+            if print_cost:
+                print("Cost after iteration {}: {}".format(i, cost))
+            model["w"] = w
+            model['b'] = b
+            with open(model_path, 'wb') as out_file:
+                np.save(out_file, model)
+                out_file.close()
+                print("Model saved!")
+    params = {"w":w,
+              "b":b}
+    return params, costs
+
+def load_model(model_path):
+    with open(model_path, 'rb') as file:
+        model = np.load(file, allow_pickle=True)
+        return model[()]
 
 def predict(params, X):
     # number of samples
