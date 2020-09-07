f13b95fc4d5b46d49b2b7b57c76a10959df2c0e6
Nhan Nguyen <nguyendtnhan@gmail.com>
2020-09-08 01:07:58 +0900

    - remove retrain function

--- BBBLearning/lr/lr.py
+++ BBBLearning/lr/lr.py
@@ -1,6 +1,7 @@
 import numpy as np
 from .propagate import propagate
 from .update import update
+from ..utils.activation import *
 
 def model(size, cost_function="cross_entropy", optimizer="GradientDecent", initializer="random"):
     model = {}
@@ -8,6 +9,10 @@ def model(size, cost_function="cross_entropy", optimizer="GradientDecent", initi
     model["cost_function"] = cost_function
     model["optimizer"] = optimizer
     model["initializer"] = initializer
+    # parameter initialization
+    w, b = parameters_initialize(size, initializer)
+    model["w"] = w
+    model['b'] = b
     return model
 
 def parameters_initialize(size, initializer = "zero"):
@@ -27,57 +32,15 @@ def train(model, X, Y, iteration, learning_rate, beta1, beta2, epsilon, model_pa
     cost_function = model["cost_function"]
     optimizer = model["optimizer"]
     initializer = model["initializer"]
-
-    costs = []
-    vgrads = {"vdw":0.0,
-              "vdb":0.0}
-    sgrads = {"sdw":0.0,
-              "sdb":0.0}
-    # parameter initialization
-    w, b = parameters_initialize(size, initializer)
-    for i in range(iteration):
-        # compute gradient and cost
-        grads, cost = propagate(w, b, X, Y, cost_function)
-
-        vgrads["vdw"] = beta1*vgrads["vdw"] + (1-beta1)*grads["dw"]
-        vgrads["vdb"] = beta1*vgrads["vdb"] + (1-beta1)*grads["db"]
-        v_corrected = 1 - np.power(beta1, i)
-        
-        sgrads["sdw"] = beta2*sgrads["sdw"] + (1-beta2)*np.multiply(grads["dw"], grads["dw"])
-        sgrads["sdb"] = beta2*sgrads["sdb"] + (1-beta2)*np.multiply(grads["db"], grads["db"])
-        s_corrected = 1 - np.power(beta2, i)
-        
-        # update parameters
-        w, b = update(w, b, grads, vgrads, sgrads, learning_rate, epsilon, v_corrected, s_corrected, optimizer)
-
-        # save costs
-        if i % 100 == 0:
-            costs.append(cost)
-            if print_cost:
-                print("Cost after iteration {}: {}".format(i, cost))
-            model["w"] = w
-            model['b'] = b
-            with open(model_path, 'wb') as out_file:
-                np.save(out_file, model)
-                out_file.close()
-                print("Model saved!")
-    params = {"w":w,
-              "b":b}
-    return params, costs
-
-def retrain(model, X, Y, iteration, learning_rate, beta1, beta2, epsilon, model_path, print_cost=False):
-    size = model["size"]
-    cost_function = model["cost_function"]
-    optimizer = model["optimizer"]
-    initializer = model["initializer"]
-
-    costs = []
     vgrads = {"vdw":0.0,
               "vdb":0.0}
     sgrads = {"sdw":0.0,
               "sdb":0.0}
     w = model['w']
     b = model['b']
+    
+    costs = []
+    
     for i in range(iteration):
         # compute gradient and cost
         grads, cost = propagate(w, b, X, Y, cost_function)
@@ -100,6 +63,10 @@ def retrain(model, X, Y, iteration, learning_rate, beta1, beta2, epsilon, model_
                 print("Cost after iteration {}: {}".format(i, cost))
             model["w"] = w
             model['b'] = b
+            model['learning_rate'] = learning_rate
+            model['beta1'] = beta1
+            model['beta2'] = beta2
+            model['epsilon'] = epsilon
             with open(model_path, 'wb') as out_file:
                 np.save(out_file, model)
                 out_file.close()
@@ -114,12 +81,14 @@ def load_model(model_path):
         return model[()]
 
 def predict(params, X):
+    w = params['w']
+    b = params['b']
     # number of samples
     m = X.shape[1]
     # output init
     Y_pred = np.zeros((1, m))
     # predict
-    z = np.dot(w.T, x) + b
+    z = np.dot(w.T, X) + b
     a = sigmoid(z)
     Y_pred = (a > 0.5).astype(int)
     return Y_pred
